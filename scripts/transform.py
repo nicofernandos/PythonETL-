543ec9bf712c6817aa4ec847c1dcae5bb4d5bf78
nicofrnnd <nicofernando060@gmail.com>
2026-04-04 21:57:06 +0700

    Updated logic transformasi dan penambahan

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,34 +1,56 @@
+import os
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, sum, round
+from pyspark.sql.functions import col, sum, round, avg
+
+
+load_dotenv()
 
 def run_transform():
-    spark = SparkSession.builder \
-        .appName("KaggleToPostgress") \
-        .config("spark.jars", "/media/farhan/DATA PRIBADI/Project/pipeline/jars/postgresql-42.7.10.jar") \
-        .getOrCreate()
+    jar_path = os.getenv("JAR_PATH")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_url = os.getenv("DB_URL")
+    db_driver = os.getenv("DB_DRIVER")
+
+    spark = (SparkSession.builder
+        .appName("KaggleToPostgres")
+        .config("spark.jars", jar_path)
+        .getOrCreate())
     
-    print("[*] Memulai proses transform dengan PySpark")
+    spark.sparkContext.setLogLevel("ERROR")
     
+    print("[*] Memulai proses transformasi dengan kredensial dari .env")
+     
     raw_path = "/media/farhan/DATA PRIBADI/Project/pipeline/data/raw/*.csv"
     df = spark.read.csv(raw_path, header=True, inferSchema=True)
+    
+    df_cleaned = (df
+        .withColumnRenamed("suicides/100k pop", "suicides_100k")
+        .withColumnRenamed("country-year", "country_year")
+        .withColumnRenamed(" HDI for year", "hdi_year")
+        .withColumnRenamed("gdp_per_capita ($)", "gdp_per_capita"))
 
-    data_mart = df.withColumnRenamed("suicides/100k pop","suicides_100k") \
-                  .withColumnRenamed("country-year","country_year") \
-                  .withColumnRenamed(" HDI for year","hdi_year")
+    data_mart = (df_cleaned.groupBy("country", "year")
+        .agg(
+            sum("suicides_no").alias("total_suicides"),
+            round(avg("gdp_per_capita"), 2).alias("avg_gdp_per_capita")
+        )
+        .orderBy("country", "year"))
+
+    print("[*] Preview Data Mart (10 baris):")
+    data_mart.show(10, truncate=False)
     
-    data_mart.show(10)
-    
-    print("[*] Proses transform selesai, data siap untuk dimuat ke PostgreSQL")
-    
-    jdbc_url = "jdbc:postgresql://localhost:5432/kaggle"
     db_properties = {
-        "user": "postgres",
-        "password": "postgres",
-        "driver" : "org.postgresql.Driver"
+        "user": db_user,
+        "password": db_password,
+        "driver": db_driver
     }
     
-    data_mart.write.jdbc(url=jdbc_url, table="dm_suicide_summary", mode="overwrite", properties=db_properties)
-    print("[OK] Data Mart berhasil dibuat di PostgreSQL!")
+    print("[*] Mengirim data ke database...")
+    data_mart.write.jdbc(url=db_url, table="dm_suicide_summary", mode="overwrite", properties=db_properties)
+    
+    print("[OK] Data mart berhasil di-load menggunakan konfigurasi .env")
 
 if __name__ == "__main__":
     run_transform()
